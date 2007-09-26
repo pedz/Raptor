4102324297c5223185c9f6fd01cfdbd2eb1fa80b
pedzan <pedzan@c4f43c74-3acd-11dc-88bc-9584e4a9cacb>
2007-09-26 16:54:36 +0000

    start redoing layouts
    git-svn-id: svn://p51.austin.ibm.com/raptor/trunk@38 c4f43c74-3acd-11dc-88bc-9584e4a9cacb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,41 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def app_doctype
+    render :partial => 'layouts/shared/doctype'
+  end
+  
+  def app_header(styles = [], scripts = [])
+    render(:partial => 'layouts/shared/header',
+           :locals => {
+             :javascript_files =>  scripts,
+             :stylesheets => styles
+           })
+  end
+
+  def app_body(*divs)
+    render(:partial => 'layouts/shared/body',
+           :locals => {
+             :divs => divs
+           })
+  end
+
+  def std_scripts(*extras)
+    [ "prototype",
+      "effects",
+      "prototype",
+      "effects",
+      "dragdrop",
+      "controls",
+      "lowpro",
+      "firebug/firebug",
+      "application" ] + extras
+  end
+  
+  def std_styles(*extras)
+    [ "scaffold" ] + extras
+  end
+  
   def admin?
     session[:user].admin
   end
