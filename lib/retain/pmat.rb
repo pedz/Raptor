33bb5499bb2cff5d15414622fd7ae59608be6540
pedzan <pedzan@c4f43c74-3acd-11dc-88bc-9584e4a9cacb>
2007-11-19 23:57:13 +0000

    added qq and at
    git-svn-id: svn://p51.austin.ibm.com/raptor/trunk@64 c4f43c74-3acd-11dc-88bc-9584e4a9cacb

---
+++ lib/retain/pmat.rb
@@ -0,0 +1,12 @@
+module Retain
+  class Pmat < Sdi
+    set_fetch_request "PMAT"
+    set_required_fields(:signon, :password,
+                        :problem, :branch:, :country)
+    set_optional_fields(:addtxt_lines)
+
+    def initialize(options = {})
+      super(options)
+    end
+  end
+end
