3c57c0e81288837887b6f436d002c45091f8ae08
pedzan <pedzan@c4f43c74-3acd-11dc-88bc-9584e4a9cacb>
2008-01-28 21:32:17 +0000

    always get PMR queue hits
    git-svn-id: svn://p51.austin.ibm.com/raptor/trunk@97 c4f43c74-3acd-11dc-88bc-9584e4a9cacb

---
+++ app/helpers/retain/favorite_queues_helper.rb
@@ -0,0 +1,15 @@
+module Retain
+  module FavoriteQueuesHelper
+    # Passed a combined queue.  We do a PMCS to get just the hits --
+    # hoping it will be cheaper.  Plus, we do not cache it up.
+    def queue_hits(queue)
+      options = {
+        :queue_name => queue.queue_name,
+        :center => queue.center,
+        :h_or_s => queue.h_or_s
+      }
+      cq = Retain::Cq.new(options)
+      cq.hits
+    end
+  end
+end
