f14f70fc8bcd578733015a2f738df76c3811de0b
pedzan <pedzan@c4f43c74-3acd-11dc-88bc-9584e4a9cacb>
2008-02-08 19:59:38 +0000

    commit just so I can move stuff
    git-svn-id: svn://p51.austin.ibm.com/raptor/trunk@114 c4f43c74-3acd-11dc-88bc-9584e4a9cacb

--- app/models/retain/queue_info.rb
+++ app/models/retain/queue_info.rb
@@ -1,4 +1,6 @@
 module Retain
   class QueueInfo < ActiveRecord::Base
+    belongs_to :queue, :class_name => "Cached::Queue"
+    belongs_to :owner, :class_name => "Cached::Registration"
   end
 end
