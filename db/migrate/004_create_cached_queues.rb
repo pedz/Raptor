752e0210c1f6ebcde936c1ddd236e5f280a08311
pedzan <pedzan@c4f43c74-3acd-11dc-88bc-9584e4a9cacb>
2008-03-10 21:29:05 +0000

    temp
    git-svn-id: svn://p51.austin.ibm.com/raptor/trunk@143 c4f43c74-3acd-11dc-88bc-9584e4a9cacb

---
+++ db/migrate/004_create_cached_queues.rb
@@ -0,0 +1,19 @@
+class CreateCachedQueues < ActiveRecord::Migration
+  def self.up
+    create_table :cached_queues do |t|
+      t.string  :queue_name, :null => false, :limit => 6
+      t.string  :h_or_s,     :null => false, :limit => 1
+      t.integer :center_id,  :null => false
+      t.timestamps 
+    end
+    execute "ALTER TABLE cached_queues ADD CONSTRAINT uq_cached_queues_triple
+             UNIQUE (queue_name, h_or_s, center_id)"
+    execute "ALTER TABLE cached_queues ADD CONSTRAINT fk_cacued_queue_center_id
+             FOREIGN KEY (center_id) REFERENCES cached_centers(id)
+             ON DELETE CASCADE"
+  end
+
+  def self.down
+    drop_table :cached_queues
+  end
+end
