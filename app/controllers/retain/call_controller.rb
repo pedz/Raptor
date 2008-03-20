7828eaa3d9ec1a7b8539c91b6f920df2aa245f54
pedzan <pedzan@c4f43c74-3acd-11dc-88bc-9584e4a9cacb>
2008-03-20 01:41:48 +0000

    fixes to queue_list
    git-svn-id: svn://p51.austin.ibm.com/raptor/trunk@155 c4f43c74-3acd-11dc-88bc-9584e4a9cacb

--- app/controllers/retain/call_controller.rb
+++ app/controllers/retain/call_controller.rb
@@ -121,7 +121,8 @@ module Retain
 
       queue = call.queue
       center = queue.center
-      personal_queues = center.queues.personal_queues
+      personal_queues = center.queues.personal_queues.map(&to_param)
+      logger.info("personal_queues are #{personal_queues.inspect}")
 
       # Walk through the signatures of the PMR adding to the pmr_queue
       # list only those not seen before and do not have owners.  Note
