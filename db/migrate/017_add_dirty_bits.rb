b812ed5af2bf6b45d4aeea8aa477358b10df8fde
pedzan <pedzan@c4f43c74-3acd-11dc-88bc-9584e4a9cacb>
2008-04-18 14:46:22 +0000

    decided I wanted a dirty bit on the queues as well
    git-svn-id: svn://p51.austin.ibm.com/raptor/trunk@187 c4f43c74-3acd-11dc-88bc-9584e4a9cacb

--- db/migrate/017_add_dirty_bits.rb
+++ db/migrate/017_add_dirty_bits.rb
@@ -2,9 +2,11 @@ class AddDirtyBits < ActiveRecord::Migration
   def self.up
     add_column :cached_pmrs, :dirty, :boolean
     add_column :cached_calls, :dirty, :boolean
+    add_column :cached_queues, :dirty, :boolean
   end
 
   def self.down
+    remove_column :cached_queues, :dirty
     remove_column :cached_calls, :dirty
     remove_column :cached_pmrs, :dirty
   end
