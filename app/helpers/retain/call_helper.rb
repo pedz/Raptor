4d2194426cfc8c44d8c2e4f42b4147bdebb68e08
pedzan <pedzan@c4f43c74-3acd-11dc-88bc-9584e4a9cacb>
2009-01-30 16:30:36 +0000

    added apar draft filters
    git-svn-id: svn://p51.austin.ibm.com/raptor/trunk@413 c4f43c74-3acd-11dc-88bc-9584e4a9cacb

--- app/helpers/retain/call_helper.rb
+++ app/helpers/retain/call_helper.rb
@@ -3,6 +3,13 @@ module Retain
 
     Condor_URL = "http://p51.austin.ibm.com/condor/"
     APAR_Regexp = Regexp.new('\bi[xyz][0-9][0-9][0-9][0-9][0-9]\b', Regexp::IGNORECASE)
+    AMT_URL = "https://reports.austin.ibm.com/aparmgt/aparmgt.html?action=view&record_num="
+    # Matches "top services" initial entry of "AMT record 1234"
+    AMT_Regexp1 = Regexp.new('AMT&nbsp;record&nbsp;([0-9]+)')
+    # Matches top services entry when the APAR is created
+    AMT_Regexp2 = Regexp.new('\(AMT&nbsp;([0-9]+)\)')
+    # Matches what people more often write: "apar draft 1234"
+    AMT_Regexp3 = Regexp.new('apar&nbsp;draft&nbsp;([0-9]+)', Regexp::IGNORECASE)
 
     def ecpaat_lines(pmr)
       temp_hash = pmr.ecpaat
@@ -36,18 +43,26 @@ module Retain
         sig_line = Retain::SignatureLine.new(line.text)
         text_line = sig_line.to_s(tz).gsub(/ /, '&nbsp;')
       else
-        text_line = line.text.gsub(/ /, '&nbsp;')
-        # EBCDIC => UTF8      -- meaning
-        # \x32   => \x16      -- Normal Protected
-        # \x22   => \xc2 \x82 -- Normal Unprotected
-        # \x3a   => \xc2 \x9a -- High Intensity Protected
-        # \x2a   => \xc2 \x8a -- High Intensity Unprotected
-        # 
-        text_line = text_line.gsub("\x16",     '</span><span class="normal-protected">&nbsp;')
-        text_line = text_line.gsub("\xc2\x82", '</span><span class="normal-unprotected">&nbsp;')
-        text_line = text_line.gsub("\xc2\x9a", '</span><span class="intensified-protected">&nbsp;')
-        text_line = text_line.gsub("\xc2\x8a", '</span><span class="intensified-unprotected">&nbsp;')
-        text_line = text_line.gsub(APAR_Regexp, "<a href=\"#{Condor_URL}swinfos/\\0\">\\0</a>")
+        text_line = line.text.
+          # Replace spaces with non-breaking spaces.
+          # If we do it last, then the links get mucked with.
+          # Need to see if a <pre> tag could be used somehow.
+          gsub(/ /, '&nbsp;').
+          # EBCDIC => UTF8      -- meaning
+          # \x32   => \x16      -- Normal Protected
+          # \x22   => \xc2 \x82 -- Normal Unprotected
+          # \x3a   => \xc2 \x9a -- High Intensity Protected
+          # \x2a   => \xc2 \x8a -- High Intensity Unprotected
+          gsub("\x16",     '</span><span class="normal-protected">&nbsp;').
+          gsub("\xc2\x82", '</span><span class="normal-unprotected">&nbsp;').
+          gsub("\xc2\x9a", '</span><span class="intensified-protected">&nbsp;').
+          gsub("\xc2\x8a", '</span><span class="intensified-unprotected">&nbsp;').
+          # Find APARs and link them to Condor
+          gsub(APAR_Regexp, "<a href=\"#{Condor_URL}swinfos/\\0\">\\0</a>").
+          # Find references to APAR drafts
+          gsub(AMT_Regexp1, "<a href=\"#{AMT_URL}\\1\">\\0</a>").
+          gsub(AMT_Regexp2, "<a href=\"#{AMT_URL}\\1\">\\0</a>").
+          gsub(AMT_Regexp3, "<a href=\"#{AMT_URL}\\1\">\\0</a>")
       end
       render(:partial => "show_line",
              :locals => {
