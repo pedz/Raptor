ac61feab16089c12bd0d206441f3bfa9a2a3069a
Perry Smith (IBM) <pedzan@NewToy.austin.ibm.com>
2009-03-23 15:37:31 -0500

    remove ldap from list of needed gems

--- config/environment.rb
+++ config/environment.rb
@@ -33,7 +33,9 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-config.gem "activeldap", :lib => "active_ldap", :version => '1.0.1'
+
+  # Until I need ldap, lets just leave this out.
+  # config.gem "activeldap", :lib => "active_ldap", :version => '1.0.1'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
