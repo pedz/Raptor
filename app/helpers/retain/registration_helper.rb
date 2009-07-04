623ed23fec4bef1af29ec898b7db5c18d551c1c4
Perry Smith (IBM) <pedzan@newtoy.easesoftware.com>
2009-07-03 20:32:45 -0500

    Made it easy to make a link to the center from the registration page.

---
+++ app/helpers/retain/registration_helper.rb
@@ -0,0 +1,11 @@
+module Retain
+  module RegistrationHelper
+    def center_link(center)
+      if center.center == "000"
+        "None"
+      else
+        link_to center.center, combined_center_path(center)
+      end
+    end
+  end
+end
