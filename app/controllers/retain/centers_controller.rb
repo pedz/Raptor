bc6a48e1799834084a5553ec93bfd71f2874bf06
Perry Smith (IBM) <pedzan@dyn864919.austin.ibm.com>
2010-02-04 16:05:34 -0600

    Tinkered with absorbed queue list for a center

--- app/controllers/retain/centers_controller.rb
+++ app/controllers/retain/centers_controller.rb
@@ -8,7 +8,8 @@ module Retain
                        :software_center_mnemonic,
                        :center_daylight_time_flag,
                        :delay_to_time,
-                       :minutes_from_gmt                        
+                       :minutes_from_gmt,
+                       :absorbed_queue_list
                       ]
       options = {
         :group_request => [ group_request ]
