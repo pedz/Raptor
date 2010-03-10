eff0952dbf1392e0bae55e29408344d73737da17
Perry Smith (IBM) <pedzan@NewToy.austin.ibm.com>
2010-03-10 15:42:11 -0600

    Minor changes because registrations were not getting updated but not sure these changes will help anything. The only real change is making update time be 3 days instead of 1 week.

--- app/models/combined/registration.rb
+++ app/models/combined/registration.rb
@@ -2,7 +2,7 @@
 
 module Combined
   class Registration < Base
-    set_expire_time 1.week
+    set_expire_time 3.days
 
     set_db_keys :signon
     add_skipped_fields :signon
@@ -81,7 +81,7 @@ module Combined
       else
         cache_options = Cached::Registration.options_from_retain(retain_registration)
         cache_options.delete(:signon)
-        # logger.debug("CMB: cache_option in registration = #{cache_options.inspect}")
+        logger.debug("CMB: cache_option in registration = #{cache_options.inspect}")
         unless (c = retain_registration.software_center).blank?
           @cached.software_center = first_center = Cached::Center.find_or_new(:center => c)
         end
