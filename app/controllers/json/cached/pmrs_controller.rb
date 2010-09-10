414dae6c7512f1664c1c581fb721e6edbe3c4ed0
Perry Smith (IBM) <pedzan@dyn9041100064.austin.ibm.com>
2010-09-10 14:44:49 -0500

    start on making json controllers more interactive

--- app/controllers/json/cached/pmrs_controller.rb
+++ app/controllers/json/cached/pmrs_controller.rb
@@ -2,7 +2,30 @@ module Json
   module Cached
     class PmrsController < Retain::RetainController
       def show
-        render :json => ::Cached::Pmr.find(params[:id]).to_json(:methods => [ :last_ct_time ])
+        pmr_id = params[:id]
+        pmr_fields = pmr_id.split(',')
+        if pmr_fields.length > 1
+          problem = pmr_fields[0]
+          branch = pmr_fields[1]
+          if pmr_fields.length > 2
+            country = pmr_fields[2]
+          else
+            country = '000'
+          end
+          hash = {
+            :problem => problem,
+            :branch => branch,
+            :country => country
+          }
+          pmr = ::Cached::Pmr.find(:first, :conditions => hash)
+        else
+          pmr = ::Cached::Pmr.find(pmr_id)
+        end
+        if pmr
+          render :json => pmr.to_json(:methods => [ :last_ct_time ])
+        else
+          render :json => "Not found".to_json, :status => 404
+        end
       end
     end
   end
