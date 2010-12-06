c371635341683c97252cc0beecde42711315fa6c
Perry Smith (IBM) <pedzan@dyn864920.austin.ibm.com>
2010-12-06 13:50:30 -0600

    Add ability to get retain nodes via json

--- app/controllers/retain_nodes_controller.rb
+++ app/controllers/retain_nodes_controller.rb
@@ -3,34 +3,40 @@ class RetainNodesController < ApplicationController
 
   # GET /retain_nodes
   # GET /retain_nodes.xml
+  # GET /retain_nodes.json
   def index
     @retain_nodes = RetainNode.all
 
     respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @retain_nodes }
+      format.html  # index.html.erb
+      format.xml   { render :xml   => @retain_nodes }
+      format.json  { render :json => @retain_nodes }
     end
   end
 
   # GET /retain_nodes/1
   # GET /retain_nodes/1.xml
+  # GET /retain_nodes/1.json
   def show
     @retain_node = RetainNode.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @retain_node }
+      format.json  { render :json => @retain_node }
     end
   end
 
   # GET /retain_nodes/new
   # GET /retain_nodes/new.xml
+  # GET /retain_nodes/new.json
   def new
     @retain_node = RetainNode.new
 
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @retain_node }
+      format.json  { render :json => @retain_node }
     end
   end
 
@@ -41,6 +47,7 @@ class RetainNodesController < ApplicationController
 
   # POST /retain_nodes
   # POST /retain_nodes.xml
+  # POST /retain_nodes.json
   def create
     @retain_node = RetainNode.new(params[:retain_node])
 
@@ -49,15 +56,18 @@ class RetainNodesController < ApplicationController
         flash[:notice] = 'RetainNode was successfully created.'
         format.html { redirect_to(@retain_node) }
         format.xml  { render :xml => @retain_node, :status => :created, :location => @retain_node }
+        format.json  { render :json => @retain_node, :status => :created, :location => @retain_node }
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @retain_node.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @retain_node.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # PUT /retain_nodes/1
   # PUT /retain_nodes/1.xml
+  # PUT /retain_nodes/1.json
   def update
     @retain_node = RetainNode.find(params[:id])
 
@@ -66,15 +76,18 @@ class RetainNodesController < ApplicationController
         flash[:notice] = 'RetainNode was successfully updated.'
         format.html { redirect_to(@retain_node) }
         format.xml  { head :ok }
+        format.json  { head :ok }
       else
         format.html { render :action => "edit" }
         format.xml  { render :xml => @retain_node.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @retain_node.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /retain_nodes/1
   # DELETE /retain_nodes/1.xml
+  # DELETE /retain_nodes/1.json
   def destroy
     @retain_node = RetainNode.find(params[:id])
     @retain_node.destroy
@@ -82,6 +95,7 @@ class RetainNodesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to(retain_nodes_url) }
       format.xml  { head :ok }
+      format.json  { head :ok }
     end
   end
   
