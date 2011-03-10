21a53809ee75880d2b0ddb55e258f1671e11a8db
Perry Smith (IBM) <pedzan@newtoy.austin.ibm.com>
2011-03-10 17:10:35 -0600

    Sigh... fix logger in other sweepr.

--- app/sweepers/call_sweeper.rb
+++ app/sweepers/call_sweeper.rb
@@ -4,13 +4,13 @@ class CallSweeper < ActionController::Caching::Sweeper
   observe Cached::Call
 
   def before_update(call)
-    logger.debug("SWEEPER: call before_update called")
+    Rails.logger.debug("SWEEPER: call before_update called")
     return unless call.last_fetched_changed?
     kill_caches(call)
   end
 
   def before_destroy(call)
-    logger.debug("SWEEPER: call before_destroy called")
+    Rails.logger.debug("SWEEPER: call before_destroy called")
     kill_caches(call)
   end
 
@@ -21,7 +21,7 @@ class CallSweeper < ActionController::Caching::Sweeper
     queue = call.queue
     queue_param = "#{queue.queue_name},#{queue.h_or_s},#{queue.center.center}"
     call_param = "#{queue_param},#{call.ppg}"
-    logger.debug("SWEEPER: kill call caches for #{call_param}")
+    Rails.logger.debug("SWEEPER: kill call caches for #{call_param}")
 
     # Delete the fragments for each section for the call pages
     [ 'top', 'center', 'left', 'right' ].each do |section|
