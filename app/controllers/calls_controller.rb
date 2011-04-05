f9d9add05f3b6c22793b7bdd2b1de53e5bf9cf04
Perry Smith <pedz@easesoftware.com>
2011-04-05 14:43:37 -0500

    Add routes controller, view, and route

---
+++ app/controllers/calls_controller.rb
@@ -0,0 +1,5 @@
+class CallsController < ApplicationController
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
 ActionController::Routing::Routes.draw do |map|
+  map.calls "calls/:group/:view/:subselect", :controller => 'calls', :action => 'index'
+  map.calls "calls/:group/:view",            :controller => 'calls', :action => 'index'
+  map.calls "calls/:group",                  :controller => 'calls', :action => 'index'
+  map.calls "calls",                         :controller => 'calls', :action => 'index'
+
   map.resources(:views) do |views|
     views.resources :elements
   end
@@ -123,33 +128,5 @@ ActionController::Routing::Routes.draw do |map|
   # These Controllers are simple things just to poke retain
   # Does a PMRQQ.  Debug and test. No useful info that I can find.
   map.resources :retain_qq, :controller => 'retain/qq'
-
-  # The priority is based upon order of creation: first created ->
-  # highest priority.
-
-  # Sample of regular route:
-  # map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  # map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  # map.resources :products
-
-  # Sample resource route with options:
-  # map.resources :products, :member => { :short => :get, :toggle => :post },
-  #    :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  # map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-
-  # You can have the root of your site routed with map.root --
-  # just remember to delete public/index.html.
   map.root :controller => "welcome"
-
-  # Install the default routes as the lowest priority.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
 end
