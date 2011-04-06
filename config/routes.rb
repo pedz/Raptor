23c0ba4775cc098ecca0842332b97320c09c4044
Perry Smith <pedz@easesoftware.com>
2011-04-06 14:02:42 -0500

    Added names scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 ActionController::Routing::Routes.draw do |map|
+  map.resources :names
+
   map.calls "calls/:group/:view/:subselect", :controller => 'calls', :action => 'index'
   map.calls "calls/:group/:view",            :controller => 'calls', :action => 'index'
   map.calls "calls/:group",                  :controller => 'calls', :action => 'index'
