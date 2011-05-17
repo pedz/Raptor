42ece51a9a238421a3dc13f7d7cfca31a4993ee7
Perry Smith <pedz@easesoftware.com>
2011-05-17 13:07:21 -0500

    Check Point -- above to move things around a bit

--- app/controllers/json/entities_controller.rb
+++ app/controllers/json/entities_controller.rb
@@ -1,18 +1,20 @@
 module Json
   class EntitiesController < ApplicationController
-    logger.debug("LOADED")
-
     def index
       group = params[:group]
       levels = params[:levels]
       filter = params[:filter]
       logger.debug("group=#{group} levels=#{levels}, filter=#{filter}")
       group = Entity.find_by_name(group)
-      level = Entity.find_by_name(level)
+      levels = Entity.find_by_name(levels)
       filter = Entity.find_by_name(filter)
+      logger.debug(group.inspect)
+      logger.debug(levels.inspect)
+      logger.debug(filter.inspect)
 
-      @entities = Nesting.find_all_by_name(params[:group], :conditions => "(#{level.item.condition}) AND (#{filter.item.condition})")
-      render :text => "hi", :layout => false
+      @nestings = group.item.nestings.find(:all, :conditions => "#{levels.item.condition.sql}")
+      logger.debug(@nestings.inspect)
+      render :json => @nestings.map(&:item)
     end
   end
 end
