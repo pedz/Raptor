d47e10e3a6d41bfc261493f09b039d691a5d8df5
Perry Smith <pedz@easesoftware.com>
2011-05-24 13:51:54 -0500

    Removed debug

--- app/controllers/json/calls_controller.rb
+++ app/controllers/json/calls_controller.rb
@@ -8,18 +8,12 @@ module Json
       group = params[:group]
       levels = params[:levels]
       filter = params[:filter]
-      logger.debug("group=#{group} levels=#{levels}, filter=#{filter}")
       group = Entity.find_by_name(group)
       levels = Entity.find_by_name(levels)
       filter = Entity.find_by_name(filter)
-      logger.debug(group.inspect)
-      logger.debug(levels.inspect)
-      logger.debug(filter.inspect)
 
       nestings = group.item.nestings.find(:all, :conditions => "(#{levels.item.condition.sql}) AND (item_type = 'Cached::Queue')")
-      logger.debug("HI")
       queue_ids = nestings.map(&:item_id)
-      logger.debug("THERE")
       calls = Cached::Call.scoped(:conditions => { :queue_id => queue_ids }).scoped(:include => :pmr).scoped(:conditions => filter.item.condition.sql)
       render :json => calls
     end
