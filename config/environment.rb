28f8485dc3a02a953942f7389a9cf268e272b45a
Perry Smith <pedz@easesoftware.com>
2011-06-12 13:21:46 -0500

    Added widget initializer

--- config/environment.rb
+++ config/environment.rb
@@ -66,14 +66,14 @@ Rails::Initializer.run do |config|
     :secret      => '15bbeb7de57ae9d03ea579825288591c'
   }
 
-  config.after_initialize do
-    AsyncObserver::Queue.queue = Beanstalk::Pool.new(['127.0.0.1:11300'])
-    
-    # This value should change every time you make a release of your app.
-    AsyncObserver::Queue.app_version = "1.0.1"
-
-    AsyncObserver::Worker.handle = proc do |job|
-      AsyncRequest.perform(job)
-    end
-  end
+  #
+  # Moving away from beanstalkd to Delayed Job (I think :-)
+  # config.after_initialize do
+  #   AsyncObserver::Queue.queue = Beanstalk::Pool.new(['127.0.0.1:11300'])
+  #   # This value should change every time you make a release of your app.
+  #   AsyncObserver::Queue.app_version = "1.0.1"
+  #   AsyncObserver::Worker.handle = proc do |job|
+  #     AsyncRequest.perform(job)
+  #   end
+  # end
 end
---
+++ config/initializers/widgets.rb
@@ -0,0 +1,24 @@
+#
+# At start up, we need to make sure that a copy of the code of each
+# widget is in the javascript/widgets directory.  It might be much
+# better to have a controller with views and use page caching.  But
+# that is fairly complex as well.
+#
+Widget.find(:all).each do |widget|
+  name = ActionView::Helpers::AssetTagHelper::JAVASCRIPTS_DIR + "/widgets/" + widget.name + ".js"
+  need_file = false
+  begin
+    stat = File::Stat.new(name)
+    need_file = (widget.updated_at > stat.mtime)
+    Rails.logger.debug stat.mtime
+    Rails.logger.debug widget.updated_at
+    Rails.logger.debug (widget.updated_at > stat.mtime).to_s
+  rescue
+    need_file = true
+  end
+  if need_file
+    File.open(name, "w") do |f|
+      f.write("Raptor.widgets.#{widget.name} = #{widget.code}")
+    end
+  end
+end
