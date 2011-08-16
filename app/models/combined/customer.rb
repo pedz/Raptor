9428c6d5587bbf125713ad796df98666789dcb1d
Perry Smith <pedz@easesoftware.com>
2011-08-16 17:26:10 -0500

    Fixed bug in combined customer model where cached should have been @cached

--- app/models/combined/customer.rb
+++ app/models/combined/customer.rb
@@ -125,7 +125,7 @@ module Combined
       
       # Pull the fields we need from the cached record into an options_hash
       options_hash = Hash[ *%w{  country customer_number }.map { |field|
-                             [ field.to_sym, cached.attributes[field] ] }.flatten ]
+                             [ field.to_sym, @cached.attributes[field] ] }.flatten ]
       
       # :group_request is a special case
       group_request = self.class.combined_class.retain_fields.map { |field| field.to_sym }
