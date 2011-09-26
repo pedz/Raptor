5b82c2576209369a2f7eb248b6934d1cb6b5e2f9
Perry Smith <pedz@easesoftware.com>
2011-09-25 21:48:37 -0500

    Test with bundler -- ldap is not working

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source :gemcutter
+
+gem "rails", "~> 2.3.11"
+# gem "activeldap", :require => "active_ldap"
+gem "icu4r"
+gem "beanstalk-client"
+gem "pg"
+
+group :development do
+  # gem "rails-footnotes", :lib => "rails-footnotes", :source => "http://gems.github.com"
+  gem "jasmine"
+end
+
+group :test do
+  gem "jasmine"
+end
--- config/environment.rb
+++ config/environment.rb
@@ -31,9 +31,6 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "activeldap", :lib => "active_ldap"
-  config.gem "icu4r"
-  config.gem "beanstalk-client"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
