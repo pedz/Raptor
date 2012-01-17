b4e3bfab44d426b14fac267df64ac97f0cc9d82d
Perry Smith <pedz@easesoftware.com>
2012-01-17 17:11:00 -0600

    Fixed new CSPO format

--- tools/process-cspo.rb
+++ tools/process-cspo.rb
@@ -16,25 +16,30 @@ cmt = Regexp.new('CMT #')
 pmrs = Regexp.new('PMRS')
 state = :new
 rnum = 0
-column = nil
+crit_column = nil
+pmr_column = nil
 
 sheet.each do |row|
   rnum += 1
   case state
   when :new
-    if cmt.match(row[0])
-      row.each_index { |index| column = index if pmrs.match(row[index]) }
-      state = :going
+    ( 0 .. 9 ).each do |i|
+      if cmt.match(row[i])
+        crit_column = i
+	row.each_index { |index| pmr_column = index if pmrs.match(row[index]) }
+	state = :going
+      end
     end
 
   when :going
-    next if row[column].nil?
-    pmr_pieces = row[column].split(',')
+    next if row[pmr_column].nil?
+    pmr_pieces = row[pmr_column].split(',')
     pmrs = (0 ... (pmr_pieces.length / 3)).inject([]) do |memo, index|
       i = index * 3
       pmr = pmr_pieces[i] + "," + pmr_pieces[i + 1] + "," + pmr_pieces[i + 2]
       memo << pmr
     end
-    puts "#{row[0]}: #{pmrs.join(' ')}"
+    puts "#{row[crit_column]}: #{pmrs.join(' ')}"
   end
 end
+
