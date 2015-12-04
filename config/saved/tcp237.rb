c8b74d49c4c83e9739880de71474987eba39111d
Perry Smith <pedz@easesoftware.com>
2015-12-04 11:35:45 -0600

    tcp237 is tunnelled again

---
+++ config/saved/tcp237.rb
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2011 Ease Software, Inc. and Perry Smith
+# All Rights Reserved
+#
+# Retain Config file for tcp237
+module Retain
+  TUNNELLED = true
+  BASE_PORT = 5000
+end
